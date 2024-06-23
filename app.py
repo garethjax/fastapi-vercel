417997b9cdf18c163635992c7ae78020b9218367
andrea scarpetta <username@users.noreply.github.com>
2024-06-23 20:14:24 +0200

    cambiamenti al codice

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ limits = httpx.Limits(max_keepalive_connections=5, max_connections=10)
 timeout = httpx.Timeout(timeout=5.0, read=15.0)
 client = httpx.AsyncClient(limits=limits, timeout=timeout)
 
+#aggiungo un commento a cazzo
 
 @app.on_event("shutdown")
 async def shutdown_event():
